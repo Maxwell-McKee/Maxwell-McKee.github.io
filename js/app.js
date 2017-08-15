01dfb621dcd3790f19c1cdd89332bfec4ecbb9b5
Max McKee <ibathow368@live.com>
2017-08-14 18:00:29 -0700

    Finished homepage. Need to add image.

--- js/app.js
+++ js/app.js
@@ -1,15 +1,21 @@
-$( document ).ready(function() {
-  console.log("Ready!");
-  
-  $(".buttonOuter").hover(function() {
-    $(this).animate(
-      {backgroundColor: "#2F2F2F",
-       top: "+=5px"},
-      100, function() {}
-    )},              function() {
-    $(this).animate(
-      {backgroundColor: "#000000",
-       top:"-=5px"},
-      100, function() {}     
-    )})
+$(document).ready(function() {
+    console.log("Ready!");
+
+    $(".buttonOuter").hover(function() {
+        $(this).animate({
+                backgroundColor: "#2F2F2F",
+                top: "-=2px"
+            },
+            100,
+            function() {}
+        )
+    }, function() {
+        $(this).animate({
+                backgroundColor: "#000000",
+                top: "+=2px"
+            },
+            100,
+            function() {}
+        )
+    })
 });
