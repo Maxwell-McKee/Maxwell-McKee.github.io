8d331b270410079bd75988367b81d5617d0e260d
Max McKee <ibathow368@live.com>
2017-08-16 14:58:33 -0700

    Scaling

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
     console.log("Ready!");
 
+    var scale = screen.height/912.0;
+    var scaleString = "scale(" + scale + ")"
+    var pct = (scale - 1) * 200 + 10;
+    var pctString = pct.toString() + "% 0 0";
+
+    $("body").css("transform",scaleString);
+    $("body").css("transform-origin", pctString);    
+
     $(".buttonOuter").hover(function() {
         $(this).animate({
                 backgroundColor: "#2F2F2F",
